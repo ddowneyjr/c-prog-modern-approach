c41453a908847111999a40eb5533b04086188864
Derrell Downey Jr <ddowneyjr@mbp.lan>
2026-02-25 14:11:47 -0500

    chapter8 files

--- chapter8/reverse.c
+++ chapter8/reverse.c
@@ -4,21 +4,20 @@
 #define N 10
 
 int main(void) {
-    int a[N], i;
+  int a[N], i;
 
-    printf("Enter %d numbers: ", N);
+  printf("Enter %d numbers: ", N);
 
-    for (i=0; i < N; i++) {
-        scanf("%d", &a[i]);
-    }
+  for (i = 0; i < N; i++) {
+    scanf("%d", &a[i]);
+  }
 
+  printf("In reverse order:");
+  for (i = N - 1; i >= 0; i--) {
+    printf(" %d", a[i]);
+  }
 
-    printf("In reverse order:");
-    for (i = N - 1; i>= 0; i--) {
-        printf(" %d", a[i]);
-    }
+  printf("\n");
 
-    printf("\n");
-
-    return 0;
+  return 0;
 }
